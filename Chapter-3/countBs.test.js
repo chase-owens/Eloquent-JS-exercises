551732bc7b5d252cb2670ad8c6354301a4714476
Chase-Owens <chasejonathanowens@gmail.com>
2018-10-23 20:53:04 -0500

    Update CountChar

--- Chapter-3/countBs.test.js
+++ Chapter-3/countBs.test.js
@@ -11,10 +11,35 @@ function countBs(word) {
   return bCount;
 }
 
+function countChar(char) {
+  return word => {
+    if (word == !String) {
+      return "Enter a word";
+    }
+    let charCount = 0;
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === char) {
+        charCount += 1;
+      }
+    }
+    return charCount;
+  };
+}
+
+let countPs = countChar("P");
+
 describe("countBs", () => {
   test("counts uppercase Bs", () => {
     expect(countBs("BoB")).toBe(2);
     expect(countBs("babel")).toBe(0);
     expect(countBs(!String)).toMatch(/Enter a word/);
+  });
+});
+
+describe("countPs", () => {
+  test("counts uppercase Ps", () => {
+    expect(countPs("PoP")).toBe(2);
+    expect(countPs("poppy")).toBe(0);
+    expect(countPs(!String)).toMatch(/Enter a word/);
   });
 });
